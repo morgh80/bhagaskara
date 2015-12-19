f11b49d435e457e351a783ca3bcd10c043c702bd
Aeronaut <aeronaut@MBPr-Aeronaut-2.local>
2015-12-19 20:40:24 +0100

    Finished charts changing function. JQ

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 $(document).ready(function(){
 
-
-    //
     function stickyMenu() {
         var menu = $(".menu");
         var menuOffsetFromTop = menu.position().top;
@@ -48,53 +46,84 @@ $(document).ready(function(){
         },3000);
     }
 
-    //Buttons skipping to next person
-
+    //Buttons skipping to next profile
     function skipButtons(){
 
-        var buttonRight = $("#arrow-right");
-        var buttonLeft = $("#arrow-left");
         var profilesParent = $(".profiles");
         var profiles = profilesParent.children();
-
         var selectedProfile = 1;
 
+        //Skip buttons var
+        var buttonRight = $("#arrow-right");
+        var buttonLeft = $("#arrow-left");
+
+        //Chart var
+        var webChart=$("#web");
+        var htmlChart=$("#html");
+        var designChart=$("#design");
+        var uxChart=$("#ux");
+
+        var webChartValue =  $(webChart.parent().children().children().next());
+        var htmlChartValue =  $(htmlChart.parent().children().children().next());
+        var designChartValue =  $(designChart.parent().children().children().next());
+        var uxChartValue =  $(uxChart.parent().children().children().next());
+
+        //Fuctions to add and remove skill classes
+
+        function addSkillClasses() {
+            webChart.addClass("percentage-" + $(profiles[selectedProfile]).data("web"));
+            webChartValue.text($(profiles[selectedProfile]).data("web") + "%");
+            htmlChart.addClass("percentage-" + $(profiles[selectedProfile]).data("html"));
+            htmlChartValue.text($(profiles[selectedProfile]).data("html") + "%");
+            designChart.addClass("percentage-" + $(profiles[selectedProfile]).data("design"));
+            designChartValue.text($(profiles[selectedProfile]).data("design") + "%");
+            uxChart.addClass("percentage-" + $(profiles[selectedProfile]).data("ux"));
+            uxChartValue.text($(profiles[selectedProfile]).data("ux") + "%");
+        };
+
+        function removeSkillClasses() {
+            webChart.removeClass("percentage-" + $(profiles[selectedProfile]).data("web"));
+            htmlChart.removeClass("percentage-" + $(profiles[selectedProfile]).data("html"));
+            designChart.removeClass("percentage-" + $(profiles[selectedProfile]).data("design"));
+            uxChart.removeClass("percentage-" + $(profiles[selectedProfile]).data("ux"));
+        };
+
         profiles[selectedProfile].classList.add("selectedProfile");
+        addSkillClasses();
 
-           buttonRight.on("click", function(event){
-                    profiles[selectedProfile].classList.remove("selectedProfile");
-                    selectedProfile++;
-                    if (selectedProfile >= profiles.length) {
-                        selectedProfile=0;
-                        profiles[selectedProfile].classList.add("selectedProfile");
-                    } else {
-                        profiles[selectedProfile].classList.add("selectedProfile");
-                    }
-            });
-
+        buttonRight.on("click", function(event){
+            profiles[selectedProfile].classList.remove("selectedProfile");
+            removeSkillClasses();
+            selectedProfile++;
+            if (selectedProfile >= profiles.length) {
+                selectedProfile=0;
+                profiles[selectedProfile].classList.add("selectedProfile");
+                addSkillClasses();
+            } else {
+                profiles[selectedProfile].classList.add("selectedProfile");
+                addSkillClasses();
+            }
+        });
 
         buttonLeft.on("click", function(event){
             profiles[selectedProfile].classList.remove("selectedProfile");
+            removeSkillClasses();
             selectedProfile--;
             if (selectedProfile < 0) {
                 selectedProfile = (profiles.length-1);
                 profiles[selectedProfile].classList.add("selectedProfile");
+                addSkillClasses();
             } else {
                 profiles[selectedProfile].classList.add("selectedProfile");
+                addSkillClasses();
             }
         });
+
     };
 
-
-
     skipButtons();
-
-
-
-
     moveSlides();
     stickyMenu();
-    //console.log("bla");
 
 
 
